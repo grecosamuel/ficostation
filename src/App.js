18f9584dbc939d4327ecde11c811b11c00540ee9
Samuel Greco <samuel.workenv@gmail.com>
2022-12-15 12:49:44 +0100

    Update

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import Home from './components/Home'
 function App() {
   return (
     <div className="App">
-      <h1>Funziona</h1>
       <Home />
     </div>
   );
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,23 @@
-import React from "react"
+import './Home.css';
+import { useRef } from 'react';
 export default function Home(){
+    const videoRef = useRef(null);
+
+    const handlePlay = () => {
+        const elem = videoRef.current;
+        if(elem){ //checking if ref was initiated
+            if (elem.requestFullscreen) {
+              elem.requestFullscreen();
+            } else if (elem.webkitRequestFullscreen) { /* Safari */
+              elem.webkitRequestFullscreen();
+            } else if (elem.msRequestFullscreen) { /* IE11 */
+              elem.msRequestFullscreen();
+            }
+          }
+    }
     return(
-       <h1>Home</h1>
+        <video autoPlay controls className="videoIntro" ref={videoRef} onPlay={handlePlay}>
+            <source src="/ficostation/ficostationIntro_QcZSSifZ.mp4" type="video/mp4"/>
+        </video>
     )
 };
