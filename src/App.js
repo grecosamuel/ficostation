007268f8cc6839c9400b0a2a8c6b1c90683b37a5
Samuel Greco <samuel.workenv@gmail.com>
2022-12-15 11:44:42 +0100

    Update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,8 @@
-import Home from "./components/Home";
-import React from "react";
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home />,
-  },
-]);
-
 
 function App() {
   return (
     <div className="App">
-     <React.StrictMode>
-      <RouterProvider router={router} />
-    </React.StrictMode>
+      <h1>Funziona</h1>
     </div>
   );
 }
